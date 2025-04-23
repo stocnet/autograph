7848cf2ee48002ab94a86e3a89ae327c36d78e51
James Hollway <james.hollway@graduateinstitute.ch>
2025-04-23 11:25:06 +0200

    Reexported necessary ggplot functions

--- R/reexports_ggplot2.R
+++ R/reexports_ggplot2.R
@@ -34,7 +34,43 @@ ggplot2::aes
 #' @export
 ggplot2::ggsave
 
+#' @importFrom ggplot2 geom_smooth
+#' @export
+ggplot2::geom_smooth
+
+#' @importFrom ggplot2 geom_point
+#' @export
+ggplot2::geom_point
+
 #' @importFrom ggplot2 scale_size
 #' @export
 ggplot2::scale_size
+
+#' @importFrom ggplot2 scale_colour_hue
+#' @export
+ggplot2::scale_colour_hue
+
+#' @importFrom ggplot2 scale_x_continuous
+#' @export
+ggplot2::scale_x_continuous
+
+#' @importFrom ggplot2 theme_grey
+#' @export
+ggplot2::theme_grey
+
+#' @importFrom ggplot2 theme
+#' @export
+ggplot2::theme
+
+#' @importFrom ggplot2 element_blank
+#' @export
+ggplot2::element_blank
+
+#' @importFrom ggplot2 element_text
+#' @export
+ggplot2::element_text
+
+#' @importFrom ggplot2 unit
+#' @export
+ggplot2::unit
 
