cb38942d7e07643772de618d1e2e4d50a468247f
James Hollway <james.hollway@graduateinstitute.ch>
2025-04-23 11:24:50 +0200

    Added palette functions

---
+++ R/map_palettes.R
@@ -0,0 +1,20 @@
+#' @export
+ag_qualitative <- function(number){
+  snet_colors <- getOption("snet_cat", default = "default")
+  colorRampPalette(snet_colors)(number)
+}
+
+#' @export
+ag_sequential <- function(number){
+  snet_colors <- getOption("snet_highlight", default = "default")
+  if(length(snet_colors)==1) snet_colors <- c("white", snet_colors[1])
+  colorRampPalette(snet_colors)(number)
+}
+
+#' @export
+ag_divergent <- function(number){
+  snet_colors <- getOption("snet_highlight", default = "default")
+  if(length(snet_colors)==2) 
+    snet_colors <- c(snet_colors[1], "white", snet_colors[2])
+  colorRampPalette(snet_colors)(number)
+}
