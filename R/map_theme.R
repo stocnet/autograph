8bf1ce6851b7187094d40ed7d8af64ed23ba59f9
James Hollway <james.hollway@graduateinstitute.ch>
2025-04-23 11:23:27 +0200

    Added theme categoricals

--- R/map_theme.R
+++ R/map_theme.R
@@ -76,3 +76,44 @@ set_background_theme <- function(theme){
   }
 }
 
+set_categorical_theme <- function(theme){
+  if(theme == "bw"){
+    options(snet_cat = c("#CCCCCC", "#000000"))
+  } else if(theme == "iheid"){
+    options(snet_cat = c("#006564","#0094D8","#622550",
+                         "#268D2B","#3E2682","#820C2B",
+                         "#008F92","#006EAA","#A8086E"))
+  } else if(theme == "ethz"){
+    options(snet_cat = c("#215CAF","#007894","#627313",
+                         "#8E6713","#B7352D","#A7117A","#6F6F6F"))
+  } else if(theme == "uzh"){
+    options(snet_cat = c("#0028A5","#4AC9E3","#A4D233",
+                         "#FFC845","#FC4C02","#BF0D3E",
+                         "#BDC9E8","#DBF4F9","#ECF6D6",
+                         "#FFF4DA","#FFDBCC","#FBC6D4",
+                         "#7596FF","#B7E9F4","#DBEDAD",
+                         "#FFE9B5","#FEB799","#F78CAA",
+                         "#3062FF","#92DFEE","#C8E485",
+                         "#FFDE8F","#FE9367","#F3537F",
+                         "#001E7C","#1EA7C4","#7CA023",
+                         "#F3AB00","#BD3902","#8F0A2E",
+                         "#001452","#147082","#536B18",
+                         "#A27200","#7E2601","#60061F"))
+    
+  } else if(theme == "rainbow"){
+    options(snet_cat = c('#E8ECFB', '#D9CCE3', '#D1BBD7', 
+                         '#CAACCB', '#BA8DB4', '#AE76A3', 
+                         '#AA6F9E', '#994F88', '#882E72', 
+                         '#1965B0', '#437DBF', '#5289C7', 
+                         '#6195CF', '#7BAFDE', 
+                         '#4EB265', '#90C987', '#CAE0AB', 
+                         '#F7F056', '#F7CB45', '#F6C141', 
+                         '#F4A736', '#F1932D', '#EE8026', 
+                         '#E8601C', '#E65518', '#DC050C', 
+                         '#A5170E', '#72190E', '#42150A'))
+  } else {
+    options(snet_cat = c("#1B9E77","#4575b4","#d73027",
+                         "#66A61E","#E6AB02","#D95F02","#7570B3",
+                         "#A6761D","#E7298A","#666666"))
+  }
+}
