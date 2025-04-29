891687b46ee65f9a9fe4ba8fec3944a53639816d
James Hollway <james.hollway@graduateinstitute.ch>
2025-04-29 10:09:44 +0200

    Improved plot.sienaGOF() to rely on ggplot2 instead of lattice

--- R/map_plot_rsiena.R
+++ R/map_plot_rsiena.R
@@ -156,7 +156,12 @@ plot.influenceTable <- function(x, separation=0, ...){
 
 # Goodness of Fit ####
 
-#' @importFrom lattice trellis.par.get trellis.par.set bwplot panel.violin
+#' SIENA Goodness of Fit
+#' @description
+#'   This function plots goodness of fit objects created using RSiena.
+#'   Unlike the plot method included in the `{RSiena}` package,
+#'   this function utilises `{ggplot2}` and not `{lattice}`,
+#'   which makes the output more compatible and themeable.
 #' @examples
 #' mynet <- sienaDependent(array(c(s501, s502), dim=c(50, 50, 2)))
 #' mybeh <- sienaDependent(s50a[,1:2], type="behavior")
@@ -172,8 +177,8 @@ plot.influenceTable <- function(x, separation=0, ...){
 #'                  varName="mynet")
 #' plot(gofi)
 #' @export
-plot.sienaGOF <- function(x, center = FALSE, scale = FALSE, violin = TRUE, key = NULL, 
-                          perc = 0.05, period = 1, position = 4, fontsize = 12, ...){
+plot.sienaGOF <- function(x, ...){
+  
   args <- list(...)
   if (is.null(args$main)) {
     main = paste("Goodness of Fit of", attr(x, "auxiliaryStatisticName"))
@@ -191,163 +196,36 @@ plot.sienaGOF <- function(x, center = FALSE, scale = FALSE, violin = TRUE, key =
     x <- x[[period]]
   }
   sims <- x$Simulations
-  obs <- x$Observations
-  itns <- nrow(sims)
-  n.obs <- nrow(obs)
-  screen <- sapply(1:ncol(obs), function(i) {
-    (sum(is.nan(rbind(sims, obs)[, i])) == 0)
-  }) & (diag(var(rbind(sims, obs))) != 0)
-  if (any((diag(var(rbind(sims, obs))) == 0))) {
-    cat("Note: some statistics are not plotted because their variance is 0.\n")
-    cat("This holds for the statistic")
-    if (sum(diag(var(rbind(sims, obs))) == 0) > 1) {
-      cat("s")
-    }
-    cat(": ")
-    cat(paste(attr(x, "key")[which(diag(var(rbind(sims, obs))) == 
-                                     0)], sep = ", "))
-    cat(".\n")
-  }
-  sims <- sims[, screen, drop = FALSE]
-  obs <- obs[, screen, drop = FALSE]
-  obsLabels <- round(x$Observations[, screen, drop = FALSE], 
-                     3)
   sims.min <- apply(sims, 2, min)
   sims.max <- apply(sims, 2, max)
-  sims.min <- pmin(sims.min, obs)
-  sims.max <- pmax(sims.max, obs)
-  if (center) {
-    sims.median <- apply(sims, 2, median)
-    sims <- sapply(1:ncol(sims), function(i) (sims[, i] - 
-                                                sims.median[i]))
-    obs <- matrix(sapply(1:ncol(sims), function(i) (obs[, 
-                                                        i] - sims.median[i])), nrow = n.obs)
-    sims.min <- sims.min - sims.median
-    sims.max <- sims.max - sims.median
+  obs <- x$Observations
+  no_vary <- sims.min == obs & sims.min == sims.max
+  if (any((diag(var(rbind(sims, obs))) == 0))) {
+    cli::cli_alert_info("Note: some statistics are not plotted because their variance is 0.")
+    statkeys <- attr(x, "key")[which(diag(var(rbind(sims, obs))) == 0)]
+    cli::cli_alert_info("This holds for the statistic{?s} {statkeys}.")
   }
-  if (scale) {
-    sims.range <- sims.max - sims.min + 1e-06
-    sims <- sapply(1:ncol(sims), function(i) sims[, i]/(sims.range[i]))
-    obs <- matrix(sapply(1:ncol(sims), function(i) obs[, 
-                                                       i]/(sims.range[i])), nrow = n.obs)
-    sims.min <- sims.min/sims.range
-    sims.max <- sims.max/sims.range
-  }
-  ymin <- 1.05 * min(sims.min) - 0.05 * max(sims.max)
-  ymax <- -0.05 * min(sims.min) + 1.05 * max(sims.max)
-  if (is.null(args$ylab)) {
-    ylabel = "Statistic"
-    if (center && scale) {
-      ylabel = "Statistic (centered and scaled)"
-    }
-    else if (scale) {
-      ylabel = "Statistic (scaled)"
-    }
-    else if (center) {
-      ylabel = "Statistic (center)"
-    }
-    else {
-      ylabel = "Statistic"
-    }
-  }
-  else {
-    ylabel = args$ylab
-  }
-  if (is.null(args$xlab)) {
-    xlabel = paste(paste("p:", round(x$p, 3), collapse = " "), 
-                   collapse = "\n")
-  }
-  else {
-    xlabel = args$xlab
-  }
-  if (is.null(args$cex)) {
-    cexpar <- par("cex")
-  }
-  else {
-    cexpar <- args$cex
-  }
-  if (is.null(args$cex.axis)) {
-    cexaxispar <- par("cex.axis")
-  }
-  else {
-    cexaxispar <- args$cex.axis
-  }
-  if (is.null(args$cex.main)) {
-    cexmainpar <- par("cex.main")
-  }
-  else {
-    cexmainpar <- args$cex.main
-  }
-  if (is.null(args$cex.lab)) {
-    cexlabpar <- par("cex.lab")
-  }
-  else {
-    cexlabpar <- args$cex.lab
-  }
-  if (is.null(args$cex.sub)) {
-    cexsubpar <- par("cex.sub")
-  }
-  else {
-    cexsubpar <- args$cex.sub
-  }
-  xAxis <- (1:sum(screen))
-  if (is.null(key)) {
-    if (is.null(attr(x, "key"))) {
-      key = xAxis
-    }
-    else {
-      key <- attr(x, "key")[screen]
-    }
-  }
-  else {
-    key <- key[screen]
-    if (length(key) != ncol(obs)) {
-      stop("Key length does not match the number of variates.")
-    }
-  }
-  br <- lattice::trellis.par.get("box.rectangle")
-  br$col <- 1
-  trellis.par.set("box.rectangle", br)
-  bu <- lattice::trellis.par.get("box.umbrella")
-  bu$col <- 1
-  lattice::trellis.par.set("box.umbrella", bu)
-  plot.symbol <- lattice::trellis.par.get("plot.symbol")
-  plot.symbol$col <- "black"
-  plot.symbol$pch <- 4
-  plot.symbol$cex <- cexpar
-  lattice::trellis.par.set("plot.symbol", plot.symbol)
-  lattice::trellis.par.set("axis.text", list(cex = cexaxispar))
-  lattice::trellis.par.set("par.xlab.text", list(cex = cexlabpar))
-  lattice::trellis.par.set("par.ylab.text", list(cex = cexlabpar))
-  lattice::trellis.par.set("par.main.text", list(cex = cexmainpar))
-  lattice::trellis.par.set("fontsize", list(text = fontsize))
-  panelFunction <- function(..., x = x, y = y, box.ratio) {
-    ind.lower <- max(round(itns * perc/2), 1)
-    ind.upper <- round(itns * (1 - perc/2))
-    yperc.lower <- sapply(1:ncol(sims), function(i) sort(sims[, 
-                                                              i])[ind.lower])
-    yperc.upper <- sapply(1:ncol(sims), function(i) sort(sims[, 
-                                                              i])[ind.upper])
-    if (violin) {
-      lattice::panel.violin(x, y, box.ratio = box.ratio, col = "transparent", 
-                   ...)
-    }
-    lattice::panel.bwplot(x, y, box.ratio = 0.1, fill = ag_base(), ...)
-    lattice::panel.xyplot(xAxis, yperc.lower, lty = 3, col = ag_base(), 
-                 lwd = 3, type = "l", ...)
-    lattice::panel.xyplot(xAxis, yperc.upper, lty = 3, col = ag_base(), 
-                 lwd = 3, type = "l", ...)
-    for (i in 1:nrow(obs)) {
-      lattice::panel.xyplot(xAxis, obs[i, ], col = ag_highlight(), type = "l", 
-                   lwd = 1, ...)
-      lattice::panel.xyplot(xAxis, obs[i, ], col = ag_highlight(), type = "p", 
-                   lwd = 3, pch = 19, ...)
-      lattice::panel.text(xAxis, obs[i, ], labels = obsLabels[i, 
-      ], pos = position)
-    }
-  }
-  lattice::bwplot(as.numeric(sims) ~ rep(xAxis, each = itns), horizontal = FALSE, 
-         panel = panelFunction, xlab = xlabel, ylab = ylabel, 
-         ylim = c(ymin, ymax), scales = list(x = list(labels = key), 
-                                             y = list(draw = FALSE)), main = main)
+  
+  itns <- nrow(sims)
+  n.obs <- nrow(obs)
+  sims <- sims[,!no_vary]
+  sims <- as.data.frame(sims) %>% 
+    mutate(sim = 1:nrow(sims)) %>% 
+    tidyr::pivot_longer(!sim)
+  obs <- obs[!no_vary]
+  obs <- as.data.frame(obs) %>% 
+    tidyr::pivot_longer(cols = everything()) %>% 
+    mutate(name = as.character((1:length(obs))-1))
+  
+  ggplot2::ggplot(sims, aes(x = name, y = value)) +
+    ggplot2::geom_violin(scale = "width", trim = FALSE, color = ag_base(),
+                         draw_quantiles = c(0.05,0.95)) +
+    ggplot2::geom_point(data = obs, aes(x = name, y = value),
+                        color = ag_highlight()) +
+    ggplot2::geom_line(data = obs, aes(x = name, y = value),
+                       group = 1,
+                       color = ag_highlight()) +
+    ggplot2::theme_minimal() +
+    ggplot2::labs(y = "Statistic", title = main, 
+                  x = paste("p:", round(x$p, 3), collapse = " "))
 }
