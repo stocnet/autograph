543ada2535fdb25efdb0c5b7cfef2c435d88d27f
James Hollway <james.hollway@graduateinstitute.ch>
2025-04-29 13:08:14 +0200

    Added siena gof plot testing

--- tests/testthat/test-map_plot_rsiena.R
+++ tests/testthat/test-map_plot_rsiena.R
@@ -1,13 +1,9 @@
-# library(RSiena)
-# mynet <- sienaDependent(array(c(s501, s502), dim=c(50, 50, 2)))
-# mycov  <- coCovar(s50a[,1])
-# mydata <- sienaDataCreate(mynet, mycov)
-# myeff <- getEffects(mydata)
-# myeff <- includeEffects(myeff, simX, interaction1="mycov")
-# myalgorithm <- sienaAlgorithmCreate(nsub=2, n3=100, seed=1291)
-# ans <- siena07(myalgorithm, data=mydata, effects=myeff, silent=TRUE, batch=TRUE)
-# res <- selectionTable(ans, mydata, "mynet", "mycov")
-# 
-# test_that("selection plot is the same", {
-#   expect_snapshot_file(plot(res))
+load(test_path("testdata", "gofi.RData"))
+
+test_that("GOF plot is the same", {
+  expect_snapshot_file(ggsave("gofi.png", plot(gofi)), "gofi.png")
+})
+
+# test_that("plotting degree distributions are the same", {
+#   expect_snapshot_file(ggsave("degdist.png", plot(res)), "degdist.png")
 # })
