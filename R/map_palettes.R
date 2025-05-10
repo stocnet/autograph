d1f35a2c41d9535bff86d8daaf04db9b7fa79794
James Hollway <james.hollway@graduateinstitute.ch>
2025-05-10 17:48:51 +0200

    Moved is_dark()

--- R/map_palettes.R
+++ R/map_palettes.R
@@ -1,3 +1,13 @@
+# Helper function to check whether a color is light or dark:
+is_dark <- function(hex) {
+  # Google luma formula for details.
+  luma <- 0.33 * grDevices::col2rgb(hex)[[1]] +
+    0.5 * grDevices::col2rgb(hex)[[2]] +
+    0.16 * grDevices::col2rgb(hex)[[3]]
+  isdark <- ifelse(luma < 186, TRUE, FALSE)
+  isdark
+}
+
 #' @importFrom grDevices colorRampPalette
 #' @export
 ag_base <- function(){
