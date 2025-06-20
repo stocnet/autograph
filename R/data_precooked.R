108030596dda94bba9a0a506e3e6c181584415df
James Hollway <james.hollway@graduateinstitute.ch>
2025-06-20 21:12:56 +0200

    Added precooked migraph data

---
+++ R/data_precooked.R
@@ -0,0 +1,15 @@
+#' Precooked results for demonstrating plotting
+#' @description
+#'   These are all pre-cooked results objects, saved here to save time in
+#'   testing and demonstrating how autograph plots look.
+#' @docType data
+#' @keywords datasets
+#' @name made_earlier
+#' @usage data(res_migraph_reg)
+"res_migraph_reg"
+
+#' @docType data
+#' @keywords datasets
+#' @rdname made_earlier
+#' @usage data(res_migraph_test)
+"res_migraph_test"
