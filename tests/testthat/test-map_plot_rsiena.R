f6542e4fcf69713bf60e5db3230ddeb0208fc3dd
James Hollway <james.hollway@graduateinstitute.ch>
2025-06-22 18:31:54 +0200

    Drop snapshot test from RSiena plot tests

--- tests/testthat/test-map_plot_rsiena.R
+++ tests/testthat/test-map_plot_rsiena.R
@@ -1,7 +1,7 @@
 load(test_path("testdata", "gofi.RData"))
 
 test_that("GOF plot is the same", {
-  expect_snapshot_file(ggsave("gofi.png", plot(gofi)), "gofi.png")
+  # expect_snapshot_file(ggsave("gofi.png", plot(gofi)), "gofi.png")
 })
 
 # test_that("plotting degree distributions are the same", {
