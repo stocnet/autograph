1e81ef396d6461b1fa98a384b9e28d48d40e9e0c
James Hollway <james.hollway@graduateinstitute.ch>
2025-06-22 14:45:27 +0200

    Added match_color() for matching one or more hexcodes to a palette's offerings

--- R/map_palettes.R
+++ R/map_palettes.R
@@ -58,3 +58,51 @@ ag_divergent <- function(number){
     snet_colors <- c(snet_colors[1], "white", snet_colors[2])
   colorRampPalette(snet_colors)(number)
 }
+
+#' Matching colors across palettes
+#' @param colors One or more hexcodes to match with colors from the palette.
+#' @param pal Optionally, a vector of hexcodes representing a palette in which 
+#'   to find matches.
+#'   By default, the current theme's qualitative palette is used.
+#' @examples
+#' match_color("#4575b4")
+#' @export
+match_color <- function(colors, pal) {
+  if (missing(pal)) pal <- ag_qualitative()
+  if (length(colors) > length(pal)) {
+    stop("Not enough unique colors in the palette for the input colors.")
+  }
+  
+  # Force colors to be a character vector
+  colors <- as.character(colors)
+  
+  # Convert all to Lab
+  labc <- grDevices::convertColor(t(grDevices::col2rgb(colors)), 
+                                  from = "sRGB", to = "Lab")
+  labp <- grDevices::convertColor(t(grDevices::col2rgb(pal)), 
+                                  from = "sRGB", to = "Lab")
+  
+  if (is.null(dim(labc))) labc <- matrix(labc, nrow = 1)
+  
+  # Compute the distance matrix (rows: input colors, cols: palette colors)
+  dists <- as.matrix(stats::dist(rbind(labc, labp)))
+  dists <- dists[seq_len(nrow(labc)), -(1:nrow(labc))]
+  
+  if(length(colors)==1){
+    matched <- pal[which.min(dists)]
+  } else {
+    matched <- character(nrow(labc))
+    used <- rep(FALSE, nrow(labp))
+
+    for (i in seq_len(nrow(labc))) {
+      # Mask already used palette entries
+      dists[i, used] <- Inf
+      idx <- which.min(dists[i, ])
+      matched[i] <- pal[idx]
+      used[idx] <- TRUE
+    }
+  }
+  
+  matched
+}
+
