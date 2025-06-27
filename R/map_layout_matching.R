8fdc8c014acabfb8bbffa43cbfd4ba71a57a3a16
James Hollway <james.hollway@graduateinstitute.ch>
2025-06-27 22:35:36 +0200

    layout_matching now explicit about the return value and times = 1 (but currently doesn't do anything)

--- R/map_layout_matching.R
+++ R/map_layout_matching.R
@@ -5,11 +5,12 @@
 #' @param .data Some `{manynet}` compatible network data.
 #' @param center,circular,times Extra parameters required for `{tidygraph}`
 #'   compatibility.
+#' @returns Returns a table of nodes' x and y coordinates.
 #' @export
 layout_tbl_graph_matching <- function(.data,
                                       center = NULL,
                                       circular = FALSE,
-                                      times = 1000) {
+                                      times = 1) {
   hlay <- manynet::layout_tbl_graph_hierarchy(.data)
   matchd <- manynet::as_edgelist(manynet::to_unnamed(manynet::to_matching(.data)))
   hlay[matchd$to,"x"] <- hlay[matchd$from,"x"]
