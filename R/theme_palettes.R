f7c40fab0f42337dd26f9c86750e0ca82d72d5f7
James Hollway <james.hollway@graduateinstitute.ch>
2025-08-22 18:35:23 +0200

    Added ag_*() functions for consistent palette calls

--- R/theme_palettes.R
+++ R/theme_palettes.R
@@ -8,6 +8,65 @@ is_dark <- function(hex) {
   isdark
 }
 
+#' Consistent palette calls
+#' @description
+#'   These functions assist in calling particular parts of a theme's palette.
+#'   For example, `ag_base()` will return the current theme's base or background
+#'   color, and `ag_highlight()` will return the color used in that theme to
+#'   highlight one or more nodes, lines, or such.
+#' @name ag_call
+#' @param number Integer of how many category colours to return.
+#' @returns One or more hexcodes as strings.
+#' @importFrom grDevices colorRampPalette
+#' @export
+ag_base <- function(){
+  utils::head(getOption("snet_highlight", default = "black"), n = 1)
+}
+
+#' @rdname ag_call
+#' @export
+ag_highlight <- function(){
+  utils::tail(getOption("snet_highlight", default = "red"), n = 1)
+}
+
+#' @rdname ag_call
+#' @export
+ag_positive <- function(){
+  utils::tail(getOption("snet_div", default = "#4575b4"), n = 1)
+}
+
+#' @rdname ag_call
+#' @export
+ag_negative <- function(){
+  utils::head(getOption("snet_div", default = "#d73027"), n = 1)
+}
+
+#' @rdname ag_call
+#' @export
+ag_qualitative <- function(number){
+  snet_colors <- getOption("snet_cat", default = c("#1B9E77","#4575b4","#d73027",
+                                                   "#66A61E","#E6AB02","#D95F02","#7570B3",
+                                                   "#A6761D","#E7298A","#666666"))
+  if(missing(number)) number <- length(snet_colors)
+  colorRampPalette(snet_colors)(number)
+}
+
+#' @rdname ag_call
+#' @export
+ag_sequential <- function(number){
+  snet_colors <- getOption("snet_highlight", default = "#d73027")
+  if(length(snet_colors)==1) snet_colors <- c(ag_base(), snet_colors[1])
+  colorRampPalette(snet_colors)(number)
+}
+
+#' @rdname ag_call
+#' @export
+ag_divergent <- function(number){
+  snet_colors <- getOption("snet_div", default = "default")
+  if(length(snet_colors)==2) 
+    snet_colors <- c(snet_colors[1], "white", snet_colors[2])
+  colorRampPalette(snet_colors)(number)
+}
 
 #' Matching colors across palettes
 #' @description
