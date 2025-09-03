7b367a2ad81a99c1e0edd4e9c9b58b5293f15a3b
James Hollway <james.hollway@graduateinstitute.ch>
2025-09-03 18:01:06 +0200

    Added plot.ag_gof method for centralising the actual plotting of GOF related objects across different packages

---
+++ R/plot_gof.R
@@ -0,0 +1,25 @@
+#' Plotting function for goodness-of-fit results
+#' @examples
+#' plot(res_siena_gof)
+#' @export
+plot.ag_gof <- function(x, ...){
+  obs <- x[[1]]
+  sims <- x[[2]]
+  main <- x[[3]]
+  p_value <- x[[4]]
+  
+  ggplot2::ggplot(sims, aes(x = name, y = value)) +
+    ggplot2::geom_violin(scale = "width", trim = FALSE, color = ag_base(),
+                         draw_quantiles = c(0.05,0.95)) +
+    ggplot2::geom_point(data = obs, aes(x = name, y = value),
+                        color = ag_highlight()) +
+    ggplot2::geom_text(data = obs, aes(label = value), 
+                       hjust = 0.05, nudge_x = 0.10,
+                       color = ag_highlight()) +
+    ggplot2::geom_line(data = obs, aes(x = name, y = value),
+                       group = 1,
+                       color = ag_highlight()) +
+    ggplot2::theme_minimal() +
+    ggplot2::labs(y = "Statistic", title = main, 
+                  x = if(is.null(p_value)) "" else paste("p:", round(p_value, 3), collapse = " "))
+}
