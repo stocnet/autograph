7767e158dfdc16025c1ce46898071b58b7d61bef
James Hollway <james.hollway@graduateinstitute.ch>
2025-09-05 17:08:19 +0200

    Added rsiena selection and influence table plotting tests

---
+++ tests/testthat/test-plot_rsiena.R
@@ -0,0 +1,19 @@
+# load(test_path("testdata", "gofi.RData"))
+# 
+# test_that("GOF plot is the same", {
+#   # expect_snapshot_file(ggsave("gofi.png", plot(gofi)), "gofi.png")
+# })
+
+# test_that("plotting degree distributions are the same", {
+#   expect_snapshot_file(ggsave("degdist.png", plot(res)), "degdist.png")
+# })
+
+test_that("siena selection table plotting works",{
+  p <- plot(res_siena_selection)
+  expect_s3_class(p, "ggplot")
+})
+
+test_that("siena influence table plotting works",{
+  p <- plot(res_siena_influence)
+  expect_s3_class(p, "ggplot")
+})
