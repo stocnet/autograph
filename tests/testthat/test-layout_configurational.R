542fa105445fd05a09cbea05e75fc9989cefe684
James Hollway <james.hollway@graduateinstitute.ch>
2025-09-05 20:36:23 +0200

    Using layout_configuration in the tests so that dispatching is also checked

--- tests/testthat/test-layout_configurational.R
+++ tests/testthat/test-layout_configurational.R
@@ -1,29 +1,29 @@
 test_that("layout dyad works", {
-  res <- layout_dyad(create_explicit(A,B))
+  res <- layout_configuration(create_empty(2))
   expect_equal(res, data.frame(x = c(0, 1),
                                y = c(0, 0)) )
 })
 
 test_that("layout triad works", {
-  res <- layout_triad(create_explicit(A,B,C))
+  res <- layout_configuration(create_empty(3))
   expect_equal(res, data.frame(x = c(0, 2, 4),
                                y = c(0, 3.5, 0) ) )
 })
 
 test_that("layout tetrad works", {
-  res <- layout_tetrad(create_explicit(A,B,C,D))
+  res <- layout_configuration(create_empty(4))
   expect_equal(res, data.frame(x = c(0,0,1,1),
                                y = c(0,1,0,1)) )
 })
 
 test_that("layout pentad works", {
-  res <- layout_pentad(create_explicit(A,B,C,D,E))
+  res <- layout_configuration(create_empty(5))
   expect_equal(res, data.frame(x = c(0, -0.9511, -0.5878, 0.5878, 0.9511),
                                y = c(1, 0.3090, -0.8090, -0.8090, 0.3090)) )
 })
 
 test_that("layout hexad works", {
-  res <- layout_hexad(create_explicit(A,B,C,D,E,F))
+  res <- layout_configuration(create_empty(6))
   expect_equal(res, data.frame(x = c(1, 0.5, -0.5, -1, -0.5, 0.5),
                   y = c(0, sqrt(3)/2, sqrt(3)/2, 0, -sqrt(3)/2, -sqrt(3)/2)) )
 })
