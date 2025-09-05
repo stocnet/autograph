12017c8b0897fd8a875ad56237af8c59af13d7b6
James Hollway <james.hollway@graduateinstitute.ch>
2025-09-05 10:57:40 +0200

    Added much more description to GOF documentation

--- R/plot_gof.R
+++ R/plot_gof.R
@@ -1,20 +1,55 @@
 #' Plotting goodness-of-fit results
 #' @description
-#'   These plot methods plot goodness of fit objects created using `RSiena::sienaGOF()` or 
-#'   `MoNAn::monanGOF()`.
-#'   Internally, the data is translated into a common format
-#'   and then plotted using `{ggplot2}` so that there is a consistent look and feel.
-#'   Unlike the plot method included in the `{RSiena}` package,
-#'   this function utilises `{ggplot2}` and not `{lattice}`,
-#'   which makes the output more compatible and themeable.
+#'   These plot methods plot goodness of fit objects created using 
+#'   `RSiena::sienaGOF()`, `MoNAn::monanGOF()`, or `ergm::gof()`.
+#'   Internally, the GOF object is translated into a common class (`ag_gof`),
+#'   which has its own plot method to ensure a consistent look and feel.
+#'   It is not expected that users will create `ag_gof` class objects themselves.
+#'   
+#'   The plot shows a violin plot of the distribution of statistics from the
+#'   simulations, with a boxplot inside the violin to show the interquartile range,
+#'   and dashed lines connecting the 5th and 95th percentiles.
+#'   The boxplot also shows outliers as crosses.
+#'   The observed statistics are shown as points and connected by a line.
+#'   The observed statistics are also labelled with their value.
+#'   If a p-value is available (as in the case of `RSiena::sienaGOF()`),
+#'   it is shown beneath the x-axis.
+#' @details
+#'   Since these plots methods are in `{autograph}`,
+#'   the plots are automatically themed according to the current theme
+#'   set using `stocnet_theme()`.
+#'   The function uses the highlight colour defined in the current theme
+#'   to highlight the observed statistics.
+#'   The function also uses the base colour defined in the current theme
+#'   to draw the violin and box plots.
+#'   
+#'   It is however completely customisable.
+#'   While a title is automatically generated so that the graph is informative, 
+#'   this can be customised by specifying the `main` argument in the plotting function,
+#'   or added after the fact using `{ggplot2}` functions such as
+#'   `ggtitle()` or `labs()`.
+#'   
+#'   The user can choose whether to plot the statistics cumulatively or not.
+#'   This is typically handled in `RSiena::sienaGOF()`, 
+#'   but for `MoNAn::monanGOF()` and `ergm::gof()`,
+#'   the cumulative option is handled here.
+#'   The default is to plot the non-cumulative statistics.
+#'   This is because the non-cumulative statistics are often more interpretable,
+#'   and the cumulative statistics can be obtained by setting `cumulative = TRUE`.
+#'   
+#'   The function also checks whether any of the statistics have zero variance
+#'   across the simulations, and if so, these statistics are not plotted,
+#'   with a message to the user indicating which statistics were omitted.
 #' @name plot_gof
-#' @param x An object of class "sienaGOF" or "gof.stats.monan".
+#' @param x An object of class "sienaGOF", "gof.stats.monan", or "gof.ergm".
 #' @param cumulative Logical, indicating whether the statistics should be
 #'   plotted cumulatively (default FALSE).
+#'   This is typically treated in `sienaGOF()` for `{RSiena}`,
+#'   but treated within the plotting function for `{MoNAn}` and `{ergm}`.
 #' @param ... Other parameters to be passed to the plotting function,
 #'   for example `main = "Title"` for a different title than the default.
 #' @returns A violin plot showing the distribution of statistics from the 
-#'   simulations and a line highlighting the observed statistics.
+#'   simulations and a line joining points showing the observed statistics.
 #' @references
 #' Hintze, J. L. and Nelson, R. D. 1998. 
 #' "Violin plots: A box plot-density trace synergism". 
