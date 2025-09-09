dc05a0d762a5cee06c8d96918b51523552ae4a48
James Hollway <james.hollway@graduateinstitute.ch>
2025-09-09 18:15:46 +0200

    Updated layout_valence test

--- tests/testthat/test-layout_valence.R
+++ tests/testthat/test-layout_valence.R
@@ -1,8 +1,8 @@
 test_that("valence layout works", {
-  edges <- data.frame(from = c("A", "B", "C", "D"),
-                      to   = c("B", "C", "D", "A"),
-                      weight = c(3, 3, 1, 1),
-                      sign = c(1, -1, 1, -1))  # 1 = positive, -1 = negative
+  edges <- data.frame(from = c("A", "B"),
+                      to   = c("B", "C"),
+                      weight = c(3, 3),
+                      sign = c(1, -1))  # 1 = positive, -1 = negative
   lo <- layout_valence(edges, times = 500)
   expect_true(sqrt(sum((lo[1,] - lo[2,])^2)) < sqrt(sum((lo[1,] - lo[3,])^2)))
 })
