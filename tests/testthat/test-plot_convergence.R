adbf8f82705a27361f89916a2302a4e5a2bff502
James Hollway <james.hollway@graduateinstitute.ch>
2025-09-11 18:45:44 +0200

    Fixed testthat syntax

--- tests/testthat/test-plot_convergence.R
+++ tests/testthat/test-plot_convergence.R
@@ -1,11 +1,11 @@
 test_that("monan diagnostics works", {
-  skip_if_os("linux") # CRAN R CMD check issue
+  skip_on_os("linux") # CRAN R CMD check issue
   p <- plot(monan_conv)
   expect_s3_class(p, "ggplot")  
 })
 
 test_that("ergm diagnostics works", {
-  skip_if_os("linux") # CRAN R CMD check issue
+  skip_on_os("linux") # CRAN R CMD check issue
   p <- plot(ergm_res)
   expect_s3_class(p, "ggplot")
 })
