dc9bbb79ae710a9d90df8b9b9b20147935673b63
James Hollway <james.hollway@graduateinstitute.ch>
2025-09-12 08:50:02 +0200

    Dropped +.ggplot wrapper to avoid node overflow issue

--- R/autograph_utilities.R
+++ R/autograph_utilities.R
@@ -19,24 +19,6 @@ thisRequiresBio <- function(pkgname) {
     }}
 }
 
-#' Add ggplot objects together
-#' @description
-#' This function allows you to add ggplot objects together using the `+` operator.
-#' This is made possible by the `{patchwork}` package.
-#' This is useful for combining multiple plots into a single figure.
-#' @param e1,e2 ggplot objects
-#' @param ... Other arguments passed to `patchwork::wrap_plots()`.
-#' @noRd
-#' @export
-`+.ggplot` <- function(e1, e2, ...) {
-  if (inherits(e2, c("ggplot", "ggplot2::ggplot"))) {
-    thisRequires("patchwork")
-    patchwork::wrap_plots(e1, e2, ...)
-  } else {
-    NextMethod()
-  }
-}
-
 seq_nodes <- function(.data){
   seq.int(manynet::net_nodes(.data))
 }
