20fc867178099a6c23e4c3451ee13c156194b36a
James Hollway <james.hollway@graduateinstitute.ch>
2026-02-26 18:45:12 +0100

    Closed #12 by using fill shapes

--- R/graph_nodes.R
+++ R/graph_nodes.R
@@ -179,6 +179,7 @@ graph_nodes <- function(p, g, node_color, node_shape, node_size) {
                                                   shape = out[["nshape"]],
                                                   size = out[["nsize"]]))
   }
+  p <- p + ggplot2::scale_shape_manual(values = c(21, 22, 24, 23, 25))
   p
 }
 
